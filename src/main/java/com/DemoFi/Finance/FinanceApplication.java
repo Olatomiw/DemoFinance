7e72bd34f80aa96312feb0011b49fa7ddb661c66
DevTom <72770851+Olatomiw@users.noreply.github.com>
2025-07-25 15:19:05 +0100

    env wahala

--- src/main/java/com/DemoFi/Finance/FinanceApplication.java
+++ src/main/java/com/DemoFi/Finance/FinanceApplication.java
@@ -8,6 +8,9 @@ public class FinanceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinanceApplication.class, args);
+		System.out.println("DB_URL = " + System.getenv("DB_URL"));
+		System.out.println("DB_USER = " + System.getenv("DB_USERNAME"));
+		System.out.println("DB_PASSWORD = " + System.getenv("DB_PASSWORD"));
 	}
 
 }
